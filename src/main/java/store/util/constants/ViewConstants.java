5ede8efb4bbc9e97553eac911af9da80a2f38e8e
Gyulguma <ybg653924@gmail.com>
2024-11-11 23:05:17 +0900

    fix($store): 영수증 출력 포맷 수정

--- src/main/java/store/util/constants/ViewConstants.java
+++ src/main/java/store/util/constants/ViewConstants.java
@@ -11,8 +11,8 @@ public class ViewConstants {
     public static final String WELCOME_MESSAGE = "안녕하세요. W편의점입니다.";
     public static final String PRODUCT_INTRODUCE_MESSAGE = "현재 보유하고 있는 상품입니다.";
     public static final String RECEIPT_HEADER = "\n==============W 편의점================";
-    public static final String RECEIPT_MIDDLE = "==============증\t정===============";
-    public static final String RECEIPT_BOTTOM = "=====================================\n";
+    public static final String RECEIPT_MIDDLE = "=============증\t정===============";
+    public static final String RECEIPT_BOTTOM = "====================================\n";
     public static final String RECEIPT_HEADER_FORMAT = "%-10s %5s %15s";
     public static final String RECEIPT_FORMAT = "%-10s %5d %15s";
     public static final String RECEIPT_BOTTOM_FORMAT = "%-10s %20s\n";
